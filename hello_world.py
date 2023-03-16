243922794f9b08d8547b82acbc23fba03a211abc
Arttu <arttu.varhe@tuni.fi>
2023-03-16 11:33:15 +0200

    Intial commit

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,7 @@
-print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello once again")
-=======
+print("Hello world")
+
 print("Hello darkness my old friend")
->>>>>>> 380b3e6c7b69204e182e6ab09da42426fb333590
-=======
+>>>>>>> 380b3e6c7b69204e182e6ab09da4242
 
 print("I've come to talk with you again")
 >>>>>>> feature/modify-print
